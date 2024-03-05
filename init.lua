e810eaa5dd618716bc64797b97b0fb1d4349328c
adamsoderstrom <adamsoderstrom@me.com>
2024-03-05 15:05:40 +0100

    null-ls mappings

--- init.lua
+++ init.lua
@@ -27,7 +27,25 @@ return {
   },
 
   lsp = {
+    config = {
+      tsserver = function(opts)
+        opts.on_attach = function(client, bufnr)
+          require("twoslash-queries").attach(client, bufnr)
+        end
+
+        return opts
+      end
+    },
     -- customize lsp formatting options
+    mappings = {
+      n = {
+        -- this mapping will only be set in buffers with an LSP attached
+	      ["<leader>ld"] = { function() vim.lsp.buf.definition() end, desc = "Jump to definition" },
+	      ["<leader>lrn"] = { function() vim.lsp.buf.rename() end, desc = "Rename" },
+        ["<leader>lrf"] = { function() vim.lsp.buf.references() end, desc = "List references" },
+        ["<leader>h"] = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
+      },
+    }, 
     formatting = {
       -- control auto formatting on save
       format_on_save = {
@@ -53,6 +71,84 @@ return {
       -- "pyright"
     },
   },
+  plugins = {
+    "jose-elias-alvarez/typescript.nvim", -- add lsp plugin
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = { "tsserver" }, -- automatically install lsp
+      },
+    },
+  },
+    -- customize lsp formatting options
+    mappings = {
+      n = {
+        -- this mapping will only be set in buffers with an LSP attached
+	      ["<leader>ld"] = { function() vim.lsp.buf.definition() end, desc = "Jump to definition" },
+	      ["<leader>lrn"] = { function() vim.lsp.buf.rename() end, desc = "Rename" },
+        ["<leader>lrf"] = { function() vim.lsp.buf.references() end, desc = "List references" },
+        ["<leader>h"] = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
+      },
+    }, 
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = true, -- enable or disable format on save globally
+        allow_filetypes = { -- enable format on save for specified filetypes only
+          -- "go",
+        },
+        ignore_filetypes = { -- disable format on save for specified filetypes
+          -- "python",
+        },
+      },
+      disabled = { -- disable formatting capabilities for the listed language servers
+        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+        -- "lua_ls",
+      },
+      timeout_ms = 1000, -- default format timeout
+      -- filter = function(client) -- fully override the default formatting function
+      --   return true
+      -- end
+    },
+    -- enable servers that you already have installed without mason
+    servers = {
+      -- "pyright"
+    },
+    -- customize lsp formatting options
+    mappings = {
+      n = {
+        -- this mapping will only be set in buffers with an LSP attached
+	      ["<leader>ld"] = { function() vim.lsp.buf.definition() end, desc = "Jump to definition" },
+	      ["<leader>lrn"] = { function() vim.lsp.buf.rename() end, desc = "Rename" },
+        ["<leader>lrf"] = { function() vim.lsp.buf.references() end, desc = "List references" },
+        ["<leader>h"] = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
+      },
+    }, 
+    formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = true, -- enable or disable format on save globally
+        allow_filetypes = { -- enable format on save for specified filetypes only
+          -- "go",
+        },
+        ignore_filetypes = { -- disable format on save for specified filetypes
+          -- "python",
+        },
+      },
+      disabled = { -- disable formatting capabilities for the listed language servers
+        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+        -- "lua_ls",
+      },
+      timeout_ms = 1000, -- default format timeout
+      -- filter = function(client) -- fully override the default formatting function
+      --   return true
+      -- end
+    },
+    -- enable servers that you already have installed without mason
+    servers = {
+      -- "pyright"
+    },
+  
 
   -- Configure require("lazy").setup() options
   lazy = {

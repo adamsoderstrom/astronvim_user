7340308da3df959327688401052b690abc369c61
adamsoderstrom <adamsoderstrom@me.com>
2024-03-05 15:04:36 +0100

    add `user` plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,61 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- Highlight, list and search todo comments in your projects
+  -- Smooth scrolling
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+    opts = {},
+  },
+
+  -- Navigate your code with search labels
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    opts = {},
+    keys = {
+      {
+        "<leader>s",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      {
+        "<leader>S",
+        mode = { "n", "o", "x" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+    },
+  }, 
+
+  -- Obsidian plugin
+  {
+    "epwalsh/obsidian.nvim",
+    version = '*',
+    lazy = true,
+    ft = 'markdown',
+    dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+    }, 
+    opts = {
+     workspaces = {
+       {
+        name = 'obsidian-0',
+        path = '~/Documents/obsidian-0' 
+       }
+     },
+     ui = {
+       enable = false
+     }
+    },
+  }
 }

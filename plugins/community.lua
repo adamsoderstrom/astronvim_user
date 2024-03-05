4e3c085081ab4d294cb42441c5e282d13b0ca26c
adamsoderstrom <adamsoderstrom@me.com>
2024-03-05 15:06:01 +0100

    misc

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.motion.mini-ai" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim"},
+   -- Vim match-up: even better `%`
+  { import = "astrocommunity.motion.vim-matchup" }, 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
